6b999d851a853c874216feb0775d2876ea9c5e0c
kwon-ga <rnjsdydry@gmail.com>
2022-10-21 19:09:39 +0900

    sequelize 디렉토리 변경

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+
+// Routes
+const userRouter = require('./user');
 // const naverUsersRouter = require('./naver-users.route.js');
 
 // router.use('/users', [naverUsersRouter]);
-
+router.use('/signup',userRouter)
 
 module.exports = router;
