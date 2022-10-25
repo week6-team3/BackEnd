3fc8f955486316c31f188d7c9c677cd4a79ed40d
Minhyeong <alsuddl25@icloud.com>
2022-10-25 20:49:12 +0900

    CORS 적용 : 일단 전부 허용 상태

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 // const {
 //   errorHandler,
@@ -10,11 +11,12 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 3000;
 
-app.use([express.json(),express.urlencoded({ extended: false }),cookieParser(),]); // body-parser 전역 미들웨어
+// 나중에 프론트에서 배포된 주소로 whitelist 추가할 것
+app.use(cors({ origin: '*' }));
+app.use([express.json(), express.urlencoded({ extended: false }), cookieParser()]); // body-parser 전역 미들웨어
 app.use('/', routes); // 라우터 등록
 // app.use(errorLogger); // Error Logger
 // app.use(errorHandler); // Error Handler
-
 
 app.listen(PORT, () => {
   console.log(PORT, '서버를 실행 중 입니다.');
